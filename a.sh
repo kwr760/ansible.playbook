afd510acee837656744936fcea55dfa0d5fc1dea
kroy <kroy760@gmail.com>
2016-03-03 22:09:50 -0800

    Stable changes

--- a.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-#sudo apt-get install -y software-properties-common
-
-#sudo add-apt-repository -y ppa:ansible/ansible
-#sudo apt-get update
-#sudo apt-get install -y ansible
-
-#sudo ansible-playbook -i /usr/local/ansible/inventories/hosts /usr/local/ansible/playbook.yml -e hostname=$1 --connection=local
--- provision.sh
+++ provision.sh
@@ -1,8 +1,1 @@
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-
-sudo add-apt-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
-
-sudo ansible-playbook -i /usr/local/ansible/inventories/hosts /usr/local/ansible/playbook.yml -e hostname=$1 --connection=local
+sudo ansible-playbook -i /usr/local/ansible/inventories/local /usr/local/ansible/provision.yml
